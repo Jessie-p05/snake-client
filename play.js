08ab6575a34e34cf623e0e294977fe6217ceb805
Jessie-p05 <jxpei05@gmail.com>
2021-04-08 01:01:31 +0000

    move the function from play.js too client.js

--- play.js
+++ play.js
@@ -1,23 +1,3 @@
 const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function () {
-  const conn = net.createConnection({
-    port: 50542,
-    host: "135.23.222.131"
-  });
-  // interpret incoming data as text
-
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  return conn;
-}
-
 console.log('Connecting ...');
 connect();
