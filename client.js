464b4aa053db34aebe37f8df109b965ab9ca8ccb
Jessie-p05 <jxpei05@gmail.com>
2021-04-08 04:11:21 +0000

    move constant value to a single file

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 const net = require('net');
-
+const { IP, PORT } = require('./constants');
 /**
  * Establishes connection with the game server
  */
 const connect = function () {
   const conn = net.createConnection({
-    port: 50542,
-    host: "135.23.222.131"
+    port: PORT,
+    host: IP
   });
   // interpret incoming data as text
 
